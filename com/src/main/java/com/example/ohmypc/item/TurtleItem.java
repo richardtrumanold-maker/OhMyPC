bb0fc0df187ef2d2c4a65eb5b8abbd649d2969a6
richardtrumanold-maker <richard.truman.old@gmail.com>
2026-06-11 18:30:29 +0300

    Delete com directory

--- com/src/main/java/com/example/ohmypc/item/TurtleItem.java
+++
@@ -1,9 +0,0 @@
-package com.example.ohmypc.item;
-
-import net.minecraft.world.item.Item;
-
-public class TurtleItem extends Item {
-    public TurtleItem(Properties properties) {
-        super(properties);
-    }
-}
--- com/src/main/java/com/example/ohmypc/network/INetworkReceiver.java
+++
@@ -1,9 +0,0 @@
-package com.example.ohmypc.network;
-
-import net.minecraft.core.BlockPos;
-
-public interface INetworkReceiver {
-    void receiveMessage(int channel, String message, INetworkReceiver sender);
-    BlockPos getPosition();
-    String getNetworkId();
-}
