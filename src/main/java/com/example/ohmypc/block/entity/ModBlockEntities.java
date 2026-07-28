f8967858ea93b4168e802ef0741d30c3bd8c04f7
RichardKashTan <richard.truman.old@gmail.com>
2026-07-28 20:20:08 +0500

    Update ModBlockEntities.java
    Updated

--- src/main/java/com/example/ohmypc/block/entity/ModBlockEntities.java
+++ src/main/java/com/example/ohmypc/block/entity/ModBlockEntities.java
@@ -9,13 +9,6 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class ModBlockEntities {
 
-    // Forward import needed
-    @SuppressWarnings("unused")
-    private static void _imports() {
-        // com.example.ohmypc.projector.CinemaProjectorBlockEntity
-    }
-
-
     public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES =
             DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, Ohmypc.MOD_ID);
 
@@ -38,9 +31,4 @@ public class ModBlockEntities {
     public static final RegistryObject<BlockEntityType<NetworkHubBlockEntity>> NETWORK_HUB =
             BLOCK_ENTITIES.register("network_hub", () ->
                     BlockEntityType.Builder.of(NetworkHubBlockEntity::new, ModBlocks.NETWORK_HUB.get()).build(null));
-
-    public static final RegistryObject<BlockEntityType<com.example.ohmypc.projector.CinemaProjectorBlockEntity>> CINEMA_PROJECTOR =
-            BLOCK_ENTITIES.register("cinema_projector", () ->
-                    BlockEntityType.Builder.of(com.example.ohmypc.projector.CinemaProjectorBlockEntity::new,
-                            ModBlocks.CINEMA_PROJECTOR.get()).build(null));
 }
